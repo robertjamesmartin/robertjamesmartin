ef959597ac6dda5cb9fca509570487859b9a73a0
Robert Martin <robertmartinbc@gmail.com>
2020-01-12 14:22:12 -0800

    Created poetry Service.

--- src/app/shared/lyrics.service.ts
+++ src/app/shared/lyrics.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LyricsService {
+  userId : string;
 
-  constructor() { }
+  constructor(
+    private afs: AngularFirestore
+  ) { }
 }
--- src/app/shared/poetry.service.ts
+++ src/app/shared/poetry.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoetryService {
+  userId: string;
 
-  constructor() { }
+  constructor(
+    private afs: AngularFirestore
+  ) { }
+
+  getPoem(poemKey) {
+    this.userId = localStorage.getItem('user');
+    return this.afs.collection('users').doc(this.userId).collection('poems').doc(poemKey).snapshotChanges();
+  }
+
+  updatePoem(poemKey, value) {
+    this.userId = localStorage.getItem('user');
+    return this.afs.collection('users').doc(this.userId).collection('poems').doc(poemKey).set(value);
+  }
+
+  deletePoem(poemKey) {
+    this.userId = localStorage.getItem('user');
+    return this.afs.collection('users').doc(this.userId).collection('poems').doc(poemKey).delete();
+  }
+
+  getPoems() {
+    this.userId = localStorage.getItem('user');
+    return this.afs.collection('users').doc(this.userId).collection('poems').snapshotChanges();
+  }
+
+  createPoem(value) {
+    this.userId = localStorage.getItem('user');
+    return this.afs.collection('users').doc(this.userId).collection('poems').add({
+      poemTitle: value.poemTitle,
+      poemGenre: value.poemGenre,
+      poemYearWritten: value.poemYearWritten,
+      description: value.description
+    })
+  }
 }
